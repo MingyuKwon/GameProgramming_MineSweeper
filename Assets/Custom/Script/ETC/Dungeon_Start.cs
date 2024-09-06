b9e6805377235698813ca721fbe99b6e888cba41
Kwon Mingyu <kmgmg230601@gmail.com>
2024-09-06 23:13:49 +0900

    initialize mine? sweeper to new floder

---
+++ Assets/Custom/Script/ETC/Dungeon_Start.cs
@@ -0,0 +1,22 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Dungeon_Start : MonoBehaviour
+{
+    public string moveAutoSceneName = "null";
+    void Start()
+    {
+
+        if(moveAutoSceneName == "null")
+        {
+            StageInformationManager.isnextStageDungeon = true;
+        }else
+        {
+            LoadingInformation.loadingSceneName = moveAutoSceneName;
+        }
+        
+
+        SceneManager.LoadScene("Loading");
+    }
+
+}
